288ce3c865957182a3b6b877cf4525b705da8038
Felipe Americano <felip.americano@gmail.com>
2020-04-18 14:56:01 -0300

    depois que eu coloquei o refectoring, parou de funcionar meu checkbox

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
 
+ before_action :set_task, only: [:show, :edit, :update, :delete]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,7 +14,6 @@ class TasksController < ApplicationController
   end
 
   def delete
-    @task = Task.find(params[:id])
     @task.destroy
 
     # no need for app/views/restaurants/destroy.html.erb
@@ -28,17 +28,19 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
 
     redirect_to task_path(@task)
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
