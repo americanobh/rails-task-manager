45026ae7de13abce72a6585ef394ab831f4a687d
Felipe Americano <felip.americano@gmail.com>
2020-04-18 14:17:32 -0300

    estou com dificuldade para editar

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.new
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@task)
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  delete 'tasks/:id', to: 'tasks#delete'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  get   'task/:id/edit', to: 'task#edit', as: :edit_task
+  patch 'tasks/:id/edit', to: 'tasks#update'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
